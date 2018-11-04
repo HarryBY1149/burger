879ea29733abf0bde52a1992c5d2fe64ce545188
H. B. Young <HarryBeckerYoung@gmail.com>
2018-11-04 11:12:07 -0500

    Trying to deploy to heroku again

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql")
 require("dotenv").config;
 
 var connection = mysql.createConnection({
-    host:process.env.db_host || "localhost",
+    host:process.env.db_host || "localhost", 
     port:process.env.db_port || 3306,
     user:process.env.db_username || "root",
     password:process.env.db_password || "Ulgo1149",
