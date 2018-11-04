2975825ffe1a7af2453ab42dbf66f5766c827663
H. B. Young <HarryBeckerYoung@gmail.com>
2018-11-04 11:04:06 -0500

    adding .env, still trying to fix heroku database

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql")
+require("dotenv").config;
 
 var connection = mysql.createConnection({
     host:process.env.db_host || "localhost",
     port:process.env.db_port || 3306,
     user:process.env.db_username || "root",
-    password:process.env.db_password || "2a4c7bd3",
-    database:process.env.db_database ||"heroku_178f3e5766e4f9d"
+    password:process.env.db_password || "Ulgo1149",
+    database:process.env.db_database || "burgerdb"
 });
 
-// mysql://ba4c0740dcd2b4:2a4c7bd3@us-cdbr-iron-east-01.cleardb.net/heroku_178f3e5766e4f9d?reconnect=true
 
 module.exports = connection
