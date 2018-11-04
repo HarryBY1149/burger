61573314842e17c00716f850c5625c587957f3e6
H. B. Young <HarryBeckerYoung@gmail.com>
2018-11-03 21:19:21 -0400

    updating database info

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 var mysql = require("mysql")
 
 var connection = mysql.createConnection({
-    host:"localhost",
+    host:"us-cdbr-iron-east-01.cleardb.net",
     port:3306,
-    user:"root",
-    password:"Ulgo1149",
-    database:"burgerdb"
+    user:"ba4c0740dcd2b4",
+    password:"2a4c7bd3",
+    database:"heroku_178f3e5766e4f9d"
 });
 
+// mysql://ba4c0740dcd2b4:2a4c7bd3@us-cdbr-iron-east-01.cleardb.net/heroku_178f3e5766e4f9d?reconnect=true
 
 module.exports = connection
